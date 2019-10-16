39c137e4e5c81d7875825ffdc7247ea25aa10655
guruu.singh2033 <guruu.singh2033@gmail.com>
2019-10-16 13:03:23 +0530

    express.static path changes
    added app.use(express.static(path.join(__dirname, '/images/registrationImages/')));
    for live changes

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '/images/')));
+
+app.use(express.static(path.join(__dirname, '/images/registrationImages/')));
 
 
 
