5d8815d5fd95d3df39680f76417fe85659799751
guruu.singh2033 <guruu.singh2033@gmail.com>
2019-12-02 15:50:12 +0530

    changes of random change localhost

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var port = normalizePort(process.env.PORT || '5000');
 // app.set('port', port);
 
 var server = http.createServer(app);
-server.listen();
+// use for random changing local host --- server.listen(5000, "127.0.0.1");
+server.listen(5000, "127.0.0.1");
 server.on('error', onError);
 server.on('listening', onListening);
 
